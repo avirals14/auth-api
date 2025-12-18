c9ee2fc67b21b40dc084831baac322ffa8996b59
avirals14 <saviher@gmail.com>
2025-12-18 14:45:13 +0530

    /me db lookup added

--- backend/src/routes/protected.routes.js
+++ backend/src/routes/protected.routes.js
@@ -1,14 +1,37 @@
 import express from "express";
 import { authMiddleware } from "../middleware/auth.middleware.js";
+import UserModel from "../models/User.model.js";
 
 const router = express.Router();
 
-router.get("/me", authMiddleware, (req, res) => {
-  return res.status(200).json({
-    success: true,
-    message: "Protected route accessed",
-    userId: req.user.userId,
-  });
+router.get("/me", authMiddleware, async (req, res) => {
+  try {
+    // get userId from middleware
+    const userId = req.user.userId;
+
+    // get user from db
+    const user = await UserModel.findById(userId).select("-password");
+
+    if(!user){
+      return res.status(404).json({
+        success:false,
+        message:"User Not Found",
+      });
+    }
+
+    // return user
+    return res.status(200).json({
+      success:true,
+      user,
+    });
+  } catch (error) {
+    console.error("/me Route Error : ", error);
+    
+    return res.status(500).json({
+      success:false,
+      message:"Failed to fetch User",
+    });
+  }
 });
 
 export default router;
